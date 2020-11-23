9367faed5c09567bf8ef4dce06945deecf181599
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-11-23 02:02:46 +0100

    Add Checkout

--- amazon-challenge/src/App.js
+++ amazon-challenge/src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header/>
-      <Home/>
-
-    </div>
+    <Router>
+      <div className="app">
+      <Header />
+        <Switch>
+        <Route path="/checkout">
+         
+          <Checkout/>
+          </Route>
+          <Route path="/">
+         
+          <Home />
+          </Route>
+          
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
