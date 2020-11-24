e581f180e70229ce52dc4bc41a74a94e8480313c
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-11-24 02:03:57 +0100

    Add Login page

--- amazon-challenge/src/App.js
+++ amazon-challenge/src/App.js
@@ -9,12 +9,16 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <h1>Login page</h1>
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
