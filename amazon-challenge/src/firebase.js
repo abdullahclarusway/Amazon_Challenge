f3ec9ef4588cf6228be14c4c0e55a068ef1aace3
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-11-24 02:17:12 +0100

    Add user authentication with Firebase

--- amazon-challenge/src/firebase.js
+++ amazon-challenge/src/firebase.js
@@ -1,9 +1,17 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBKsOpedGTy1tmBbLdfJYOgiMbvJ-mYFI8",
-    authDomain: "challenge-e2171.firebaseapp.com",
-    databaseURL: "https://challenge-e2171.firebaseio.com",
-    projectId: "challenge-e2171",
-    storageBucket: "challenge-e2171.appspot.com",
-    messagingSenderId: "627231587022",
-    appId: "1:627231587022:web:b5f2daad3b236f7d02ae9f"
-  };
+  apiKey: "AIzaSyBKsOpedGTy1tmBbLdfJYOgiMbvJ-mYFI8",
+  authDomain: "challenge-e2171.firebaseapp.com",
+  databaseURL: "https://challenge-e2171.firebaseio.com",
+  projectId: "challenge-e2171",
+  storageBucket: "challenge-e2171.appspot.com",
+  messagingSenderId: "627231587022",
+  appId: "1:627231587022:web:b5f2daad3b236f7d02ae9f",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
