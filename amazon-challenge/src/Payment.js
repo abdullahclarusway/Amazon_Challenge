1cf17721c81a141b7fa948d0986b6a8e68217cf4
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-11-26 00:32:38 +0100

    Update Payment.js and Payment.css

--- amazon-challenge/src/Payment.js
+++ amazon-challenge/src/Payment.js
@@ -1,29 +1,35 @@
 import React from "react";
-import "/Payment.css";
+import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import Checkout from "./Checkout";
 import CheckoutProduct from "./CheckoutProduct";
+import {Link} from "react-router-dom";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="payment">
-      <div className="payment_container">
-        <div className="payment_section">
-          <div className="payment_title">
+      <div className="payment__container">
+          <h1>
+              Checkout (
+              <Link to="/checkout">{basket?.length} items</Link>
+              )
+          </h1>
+        <div className="payment__section">
+          <div className="payment__title">
             <h3>Delivery Adress</h3>
           </div>
-          <div className="payment_adress">
+          <div className="payment__adress">
             <p>{user?.email}</p>
             <p>123 React Lane</p>
             <p>Los Angeles, CA</p>
           </div>
         </div>
-        <div className="payment_section">
-          <div className="payment_title">
+        <div className="payment__section">
+          <div className="payment__title">
             <h3>Review items and delivery</h3>
           </div>
-          <div className="payment_items">
+          <div className="payment__items">
             {basket.map((item) => (
               <CheckoutProduct
                 id={item.id}
@@ -33,6 +39,14 @@ function Payment() {
                 rating={item.rating}
               />
             ))}
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            <p>Details</p>
           </div>
         </div>
       </div>
